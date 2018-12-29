34554dd625422eddaaa5ca9cd68ce2236942181e
name <1165053882@qq.com>
2018-12-29 10:31:42 +0800

    dong

--- My_扫描网段.bat
+++ My_扫描网段.bat
@@ -1,6 +1,5 @@
 @echo off
-
-for /l %%i in (0,1,2) do ( ping -w 60 -n 1 X.X.X.%%i|find /i "ttl" >>ip.txt 
-)
+:: -w 超时时间 -n ping 的次数
+for /l %%i in (0,1,2) do ( ping -w 60 -n 1 X.X.X.%%i|find /i "ttl" >>ip.txt )
 
 pause
